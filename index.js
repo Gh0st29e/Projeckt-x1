b9a8ca4b9b9fb2a2d54dec9a52e3ad7bde923f31
Gh0st29e <sean.niklas.meier@gmail.com>
2026-04-02 12:20:03 +0200

    Initialize Discord bot with ping command

---
+++ index.js
@@ -0,0 +1,21 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
+
+client.once('ready', () => {
+  console.log(`Bot ist online als ${client.user.tag}`);
+});
+
+client.on('messageCreate', async (message) => {
+  if (message.content.startsWith('!ping')) {
+    message.reply('Pong!');
+  }
+});
+
+client.login(process.env.TOKEN);
